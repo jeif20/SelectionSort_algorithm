bf8ec3778c5ea867e5fafd388f4622cc5edfd9f4
Juan Andonaire <andonaire.juan@gmail.com>
2019-01-21 09:03:05 -0500

    Update main.c

--- SelectionSort_algorithm/main.c
+++ SelectionSort_algorithm/main.c
@@ -2,14 +2,43 @@
 //  main.c
 //  SelectionSort_algorithm
 //
-//  Created by Juan Andonaire on 1/20/19.
-//  Copyright © 2019 Juan Andonaire. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void selectionSort(int A[],int size);
+int i;
+
+int main(){
+    int A[5] = {6,4,12,9,2}; //elements have been places in the array
+    int size = 5; //total of 5 elements
+    printf("Elements unsorted: \n");
+    for(i=0;i<size;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+    
+    selectionSort(A,size);
+    
+    printf("Elements sorted: \n");
+    for(i=0;i<size;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+    
     return 0;
+}
+
+void selectionSort(int A[],int size){
+    int j, iMin,temp;
+    for(i=0;i<size-2;i++){
+        iMin = i;
+        for(j=i+1;j<size;j++){
+            if(A[j]<A[iMin]){
+                iMin = j;
+            }
+        }
+        temp = A[i];
+        A[i] = A[iMin];
+        A[iMin] = temp;
+    }
 }
