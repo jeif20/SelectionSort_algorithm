06fc72e0fb57ec8471916d805f0f16c7a44700c0
Juan Andonaire <andonaire.juan@gmail.com>
2019-01-20 21:56:58 -0500

    Initial Commit

---
+++ SelectionSort_algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SelectionSort_algorithm
+//
+//  Created by Juan Andonaire on 1/20/19.
+//  Copyright © 2019 Juan Andonaire. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
